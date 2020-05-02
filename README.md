32db7d324fa6b8f693069d311b9899954353a603
John Keng <johnkeng@Johnde-MacBook-Pro.local>
2020-05-02 20:34:26 +0800

    push

---
+++ README.md
@@ -0,0 +1,8 @@
+# 
+不需要 Webpack 不需要 node 與 node_modules。
+不用 compiler 也不用 babel。
+
+### run
+```console
+make
+```
---
+++ bin/www.ts
@@ -0,0 +1,28 @@
+import { Application, NotFoundException } from 'https://deno.land/x/abc@v0.2.11/mod.ts'
+import { logger } from 'https://deno.land/x/abc@v0.2.11/middleware/logger.ts'
+import { join } from 'https://deno.land/std@v0.42.0/path/mod.ts'
+
+const { readFile, transpileOnly  } = Deno
+
+const app = new Application()
+app.start({ port: 8887, hostname: '0.0.0.0' })
+console.log('Server running at http://127.0.0.1:8887/')
+
+app.use(logger()).get('/*files', async (c) => {
+
+  if (c.path === '/') {
+    return c.redirect('/home')
+  } 
+
+  const f = await readFile(join('.', c.path.slice(1)))
+  const code = new TextDecoder().decode(f)
+
+  if (c.path === '/home') {
+    return c.html(code)
+  } 
+
+  throw new NotFoundException()
+})
+
+
+
